676ffeaad25f37d32b2b0b5eab89895cb4853e88
hadi <65118892+hadiazt@users.noreply.github.com>
2021-11-28 20:36:04 +0330

    Updated `stats.js`

--- commands/stats.js
+++ commands/stats.js
@@ -17,17 +17,17 @@ module.exports = {
             .setTitle('Bot Stats')
             .setColor('#0fe694')
             .setDescription(`
-<:servers:875071118107619339> Guild Count : `+ '`' + client.guilds.cache.size + '`' + `
+<:space:874678195843125278><:airplane_emoji:914500272087236628> *Guilds Count : `+ '`' + client.guilds.cache.size + '`' + `*
 
-<:users:875070999660490812> Members Count : `+ '`' + MemberCount + '`' + `
+<:space:874678195843125278><:bustsinsilhouette_emoji:914504068020437012> *Members Count : `+ '`' + MemberCount + '`' + `*
 
-<:channels:875071030908055633> Channels Count : `+ '`' + client.channels.cache.size + '`' + `
+<:space:874678195843125278><:keycapnumbersign_emoji:914500272422789161> *Channels Count : `+ '`' + client.channels.cache.size + '`' + `*
 
-<:status:875071081088704543> Status : `+ '`' + client.user.presence.status + '`' + `
+<:space:874678195843125278><:horizontaltrafficlight_emoji:914504404818874389> *Status : `+ '`' + client.user.presence.status + '`' + `*
 
-<:ping:875071055520231485> Ping : `+ '`' + Math.round(client.ws.ping) + ' ms`' + `
+<:space:874678195843125278><:antennabars_emoji:914500272255025193> *Ping : `+ '`' + Math.round(client.ws.ping) + 'ms`' + `*
 
-<:version:875070975560003595> Version : `+ '`' + VER + '`'+`
+<:space:874678195843125278><:triangularflag_emoji:914500272779296789>*Version :  `+ '`' + VER + '`' + `*
 `)
 
         return interaction.reply({ embeds: [supportmsg] })
