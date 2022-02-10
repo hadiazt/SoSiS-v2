26f6b32356bb5781ec4b8e3338962c7107279180
hadi <65118892+hadiazt@users.noreply.github.com>
2022-02-10 14:15:19 +0330

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 // ecosystem.config.js
 module.exports = {
     apps: [
-        {name: 'SoSiS-v',max_memory_restart: '700M',script: 'index.js',args: '1'}
+        {name: 'SoSiS-v',max_memory_restart: '700M',script: 'index.js'}
     ]
   };
